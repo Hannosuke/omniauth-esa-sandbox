2fac4910b02dcbf2901f4fca8c3fa0ef486542fe
ryuhanda <r.handa@everyleaf.com>
2023-09-24 18:04:37 +0900

    omniauthの設定ファイルは不要だったので削除
    deviseを使っている場合にomniauthのイニシャライズファイルがあると次のエラーが発生する様子
    ( cf. https://stackoverflow.com/questions/22386149/csrf-detected-with-omniauth-and-google )
    Authentication failure! csrf_detected: OmniAuth::Strategies::OAuth2::CallbackError, csrf_detected | CSRF detected

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :esa,
-           Rails.application.credentials.oauth[:client_id],
-           Rails.application.credentials.oauth[:client_secret],
-           scope: 'read'
-end
