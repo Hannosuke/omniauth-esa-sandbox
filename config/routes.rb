4ad2287ebbd0577139da55e0484a33b8f9b79990
ryuhanda <r.handa@everyleaf.com>
2023-09-24 20:35:12 +0900

    ログイン・ログアウトのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to: "tasks#index"
   resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/auth/esa/callback", to: "sessions#create"
 end
